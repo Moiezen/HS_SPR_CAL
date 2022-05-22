a934c7f117f79df185ab4260e07a8c27dabb6cc7
Zhu Tianran <83269704+Moiezen@users.noreply.github.com>
2022-05-22 20:55:19 +0800

    Add files via upload

---
+++ BF.cpp
@@ -0,0 +1,334 @@
+#include <bits/stdc++.h>
+using namespace std;
+enum kpm{
+	ayb,bc,jb,sjdf,hjys,syzl,hrlq,dy,tw,glfz,ljsc,ljfs,lj
+};
+enum scm{
+	msyzl,mhrlq,mdy,mtw,mglfz,mljsc,dfyx,dfsc,nul
+};
+kpm s2k(scm a){
+	switch(a){
+		case msyzl:{
+			return syzl;
+			break;
+		}
+		case mhrlq:{
+			return hrlq;
+			break;
+		}
+		case mdy:{
+			return dy;
+			break;
+		}
+		case mtw:{
+			return tw;
+			break;
+		}
+		case mglfz:{
+			return glfz;
+			break;
+		}
+		case mljsc:{
+			return ljsc;
+			break;
+		}
+		default:{
+			assert(0);
+			break;
+		}
+	}
+}
+scm k2s(kpm a){
+	switch(a){
+		case syzl:{
+			return msyzl;
+			break;
+		}
+		case hrlq:{
+			return mhrlq;
+			break;
+		}
+		case dy:{
+			return mdy;
+			break;
+		}
+		case tw:{
+			return mtw;
+			break;
+		}
+		case glfz:{
+			return mglfz; 
+			break;
+		}
+		case ljsc:{
+			return mljsc;
+			break;
+		}
+		default:{
+			assert(0);
+			break;
+		}
+	}
+}
+//敌方英雄-可以是鬼灵匪贼的目标
+//敌方随从-可以是背刺的目标
+//空目标
+//以上虚目标均归入随从名以简化操作目标
+int bcost(scm a){
+	switch(a){
+		case msyzl:{
+			return 4;
+			break;
+		}
+		case mhrlq:{
+			return 2;
+			break;
+		}
+		case mdy:{
+			return 4;
+			break;
+		}
+		case mtw:{
+			return 2;
+			break;
+		}
+		case mglfz:{
+			return 6;
+			break;
+		}
+		case mljsc:{
+			return 20;
+			break;
+		}
+		case dfyx:{
+			return 99;
+			break;
+		}
+		case dfsc:{
+			return 88;
+			break;
+		}
+	}
+}
+struct card{
+	kpm name;
+	int cost;
+	//int health;
+};
+card cardcons(kpm a,int b){
+	card c;
+	c.name=a;
+	c.cost=b;
+	return c;
+}
+struct minion{
+	scm name;
+	//int health;
+};
+minion minioncons(scm a){
+	minion b;
+	b.name=a;
+	return b;
+} 
+//已假设 背刺只用于去除老千和腾武 而鬼灵匪贼总能去除
+struct aura{
+	int asjdf;
+	int ahrlq;
+	int ady1;
+	int ady2;
+};
+struct state{
+	vector<card> hands;
+	vector<minion> minions;
+	aura auras;
+	int mana;
+	int num;
+}emptyst;
+struct ope{
+	int cost;
+	kpm name;
+	scm target;
+};
+//由于随从暂时不区分血量，操作目标仅需要随从名，且已可以包括敌方英雄
+struct opes{
+	vector<ope> opes;
+};
+
+bool rmv1(vector<minion>&a,scm b){
+	vector<minion>::iterator i=a.begin();
+	while(i!=a.end()){
+		if((*i).name==b){
+			a.erase(i);
+			return true;
+		}
+		i++;
+	}
+	return false;
+}
+bool rmv2(vector<card>&a,kpm b,int c){
+	vector<card>::iterator i=a.begin();
+	while(i!=a.end()){
+		if((*i).name==b&&(*i).cost==c){
+			a.erase(i);
+			return true;
+		}
+		i++;
+	}
+	return false;
+}
+
+pair<state,int> badpair=make_pair(emptyst,-999999);
+
+const int hlim=10;
+const int mlim=7;
+
+const int sjdflim=1;
+const int hrlqlim=3;
+const int dy1lim=2;
+const int dy2lim=2;
+
+int twice(state st){
+	vector<minion>::iterator i=st.minions.begin();
+	while(i!=st.minions.end()){
+		if((*i).name==msyzl){
+			return 2;
+		}
+		i++;
+	}
+	return 1;
+}
+
+pair<state,int> trans(state st,ope op){
+	switch(op.name){
+		case ayb:{
+			bool flag=rmv1(st.minions,op.target);
+			if(!flag) return badpair;
+			flag=rmv2(st.hands,op.name,op.cost);
+			if(!flag) return badpair;
+			st.mana-=max(op.cost-st.auras.asjdf*2-st.auras.ady1*3,0);
+			if(st.mana<0) return badpair;
+			
+			st.hands.push_back(cardcons(s2k(op.target),bcost(op.target)-2));
+			st.auras.asjdf=0;
+			st.auras.ady1=st.auras.ady2;
+			st.auras.ady2=0;
+			st.num++;
+			break;
+		}
+		case bc:{
+			
+			break;
+		}
+		case jb:{
+			bool flag=rmv2(st.hands,op.name,op.cost)&&(op.target==nul);
+			if(!flag) return badpair;
+			st.mana-=max(op.cost-st.auras.asjdf*2-st.auras.ady1*3,0);
+			if(st.mana<0) return badpair;
+			
+			st.mana++;
+			st.auras.asjdf=0;
+			st.auras.ady1=st.auras.ady2;
+			st.auras.ady2=0;
+			st.num++;
+			break;
+		}
+		case sjdf:{
+			bool flag=rmv2(st.hands,op.name,op.cost)&&(op.target==nul);
+			if(!flag) return badpair;
+			st.mana-=max(op.cost-st.auras.asjdf*2-st.auras.ady1*3,0);
+			if(st.mana<0) return badpair;
+			
+			st.auras.asjdf=1;
+			st.auras.ady1=st.auras.ady2;
+			st.auras.ady2=0;
+			st.num++;
+			break;
+		}
+		case hjys:{
+			bool flag=rmv2(st.hands,op.name,op.cost)&&(op.target==nul);
+			if(!flag) return badpair;
+			st.mana-=max(op.cost-st.auras.asjdf*2-st.auras.ady1*3,0);
+			if(st.mana<0) return badpair;
+			
+			vector<minion>::iterator i=st.minions.begin();
+			while(i!=st.minions.end()){
+				if(st.hands.size()>=hlim) break; 
+				st.hands.push_back(cardcons(s2k((*i).name),1));
+				i++;
+			}
+			st.auras.asjdf=0;
+			st.auras.ady1=st.auras.ady2;
+			st.auras.ady2=0;
+			st.num++;
+			break;
+		}
+		case syzl:{
+			bool flag=rmv2(st.hands,op.name,op.cost)&&(op.target==nul);
+			if(!flag) return badpair;
+			if(st.minions.size()>=mlim) flag=false;
+			else st.minions.push_back(minioncons(k2s(op.name)));
+			if(!flag) return badpair;
+			st.mana-=max(op.cost-st.auras.ady1*3,0);
+			if(st.mana<0) return badpair;
+			
+			st.auras.ady1=st.auras.ady2;
+			st.auras.ady2=0;
+			st.num++;
+			break;
+		}
+		case hrlq:{
+			int tw=twice(st);
+			bool flag=rmv2(st.hands,op.name,op.cost)&&(op.target==nul);
+			if(!flag) return badpair;
+			if(st.minions.size()>=mlim) flag=false;
+			else st.minions.push_back(minioncons(k2s(op.name)));
+			if(!flag) return badpair;
+			st.mana-=max(op.cost-st.auras.ady1*3,0);
+			if(st.mana<0) return badpair;
+			
+			st.auras.ahrlq=min(st.auras.ahrlq+tw,hrlqlim);
+			st.auras.ady1=st.auras.ady2;
+			st.auras.ady2=0;
+			st.num++;
+			break;
+		}
+		case dy:{
+			bool flag=rmv2(st.hands,op.name,op.cost)&&(op.target==nul);
+			if(!flag) return badpair;
+			if(st.minions.size()>=mlim) flag=false;
+			else st.minions.push_back(minioncons(k2s(op.name)));
+			if(!flag) return badpair;
+			st.mana-=max(op.cost-st.auras.ahrlq*2-st.auras.ady1*3,0);
+			if(st.mana<0) return badpair;
+			
+			st.auras.ahrlq=0;
+			st.auras.ady1=min(st.auras.ady2+tw,dy1lim);
+			st.auras.ady2=tw;
+			st.num++;
+			break;
+		}
+		case tw:{
+			
+		}
+		case glfz:{
+			
+			break;
+		}
+		case ljsc:{
+			
+			break;
+		}
+		case ljfs:{
+			
+			break;
+		}
+		case lj:{
+			return badpair;
+			break;
+		}
+	}
+}
+
+int main(){
+	
+}
