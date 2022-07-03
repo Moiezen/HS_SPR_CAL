8e8fccf3bd69eefd4f9d7d66ed76a93780b963b6
Zhu Tianran <83269704+Moiezen@users.noreply.github.com>
2022-07-04 00:56:54 +0800

    Add files via upload

--- UI.cpp
+++ UI.cpp
@@ -86,11 +86,11 @@ string k2str(kpm a){
 			break;
 		}
 		case ljsc:{
-			return "垃圾随从"; 
+			return "随从";
 			break;
 		}
 		case ayb:{
-			return "暗影步";
+			return "暗步";
 			break;
 		}
 		case bc:{
@@ -98,19 +98,19 @@ string k2str(kpm a){
 			break;
 		}
 		case jb:{
-			return "币";
+			return "硬币";
 			break;
 		}
 		case sjdf:{
-			return "伺机待发"; 
+			return "伺机"; 
 			break;
 		}
 		case hjys:{
-			return "幻觉药水";
+			return "幻药";
 			break;
 		}
 		case ljfs:{
-			return "垃圾法术"; 
+			return "法术";
 			break;
 		}
 		case lj:{
@@ -347,11 +347,9 @@ int twice(state st){
 	return 1;
 }
 
-int tc=0;
+int openmode;
 
 pair<state,int> trans(state st,ope op){
-	tc++;
-	
 	int dmg=0;
 	bool flag=true;
 	switch(op.name){
@@ -371,12 +369,25 @@ pair<state,int> trans(state st,ope op){
 			break;
 		}
 		case bc:{
-			//暂时未考虑区分背刺敌方随从和背刺血量>2的友方随从，统一记作null，且认为总能这么做 
-			//用作去除时，仅考虑去除狐人老千和赤烟腾武 
+			//用作连击时，目标统一记作null，不区分敌方随从和血量>2的友方随从
+			//根据模式检定，openmode=1时认为总有敌方随从可以选择，直接通过
+			//openmode=0时认为没有敌方随从可以选择，仅考虑选择鲨鱼之灵，刀油和鬼灵匪贼，并认为它们总可以选择且总不会去除 
+			//用作去除时，仅考虑选择狐人老千和赤烟腾武 
 			flag=rmv2(st.hands,op.name,op.cost);
 			if(!flag) return badpair;
 			if(op.target==nul){
-				
+				if(openmode==0){
+					flag=false;
+					vector<minion>::iterator i=st.fields.begin();
+					while(i!=st.fields.end()){
+						if((*i).name==msyzl||(*i).name==mdy||(*i).name==mglfz){
+							flag=true;
+							break;
+						}
+						i++;
+					}
+					if(!flag) return badpair;
+				}
 			}
 			else{
 				flag=(op.target==mhrlq)||(op.target==mtw);
@@ -651,7 +662,7 @@ void solve_(syn q){
 	}
 }
 
-string solve(state st,int _eh=999999,int _tlim=999999){
+string solve(state st,int _eh=999,int _tlim=999999){
 	eh=_eh;
 	done=curdmg=0;
 	curos=emptyos;
@@ -663,9 +674,6 @@ string solve(state st,int _eh=999999,int _tlim=999999){
 	
 	return output(curos,curdmg,done);
 }
-
-int __eh=999999;
-int __tlim=999999;
 
 int H=640;
 int W=1280;
@@ -708,7 +716,7 @@ bool inrect(int x,int y,rect a){
 }
 
 enum domt{
-	nodomt,vnx,vx,cn,cx,go,ans
+	nodomt,vnx,vx,cn,cx,go,ans,vxa,lk,lk1,lk2,vxb
 };
 struct domain{
 	rect r;
@@ -738,14 +746,14 @@ void init(){
 }
 
 rect _all=rectcons(0,0,1280,640);
-rect _start=rectcons(1060,560,1180,640);
+rect _start=rectcons(1160,560,1280,640);
 
 void drawatt(){
-	textr(_all,txc,"点击开始使用，代表你已经阅读过https://github.com/Moiezen/HS_SPR_CAL的README.md，并且该UI.exe是直接获取的最新版本");
+	textr(_all,txc,"点击开始，代表你已经阅读过https://github.com/Moiezen/HS_SPR_CAL的README.md，并且该UI.exe是直接获取的最新版本");
 	
 	barr(_start,bdc);
 	barr(cutedge(_start,1),bgc);
-	textr(_start,txc,"开始使用");
+	textr(_start,txc,"开始");
 }
 
 void attention(){
@@ -762,6 +770,146 @@ void attention(){
         	return;
 		}
 	}
+}
+
+vector<domain> doms;
+domain selected;
+int manadomid;
+int cardsdomid[10];
+int ansdomid;
+int ehdomid;
+int tlimdomid;
+int lkdomid;
+int modedomid;
+
+domain id2dom(int id){
+	vector<domain>::iterator i=doms.begin();
+	while(i!=doms.end()){
+		if(id==(*i).id){
+			return (*i);
+			break;
+		}
+		i++;
+	}
+	return nodomain;
+}
+
+void follow(domain a){
+	vector<domain>::iterator i=doms.begin();
+	while(i!=doms.end()){
+		if(a.id==(*i).id){
+			(*i)=a;
+			break;
+		}
+		i++;
+	}
+}
+
+string n1[14]={"鱼灵","老千","刀油","鬼灵","腾武","暗步","背刺","硬币","伺机","幻药","随从","法术","垃圾","清除"};
+string customlk="D:\\Hearthstone";
+
+void getdoms(){
+	doms.clear();
+	addd(860,600,920,640,vx,"水晶",0,manadomid=1);
+	//1
+	rep(i,0,9) addd(200+i*60,600,260+i*60,640,vnx,"清除",0,cardsdomid[i]=2+i);
+	//2 ... 11
+	rep(i,0,10) addd((i+2)%3*40,400+(i+2)/3*40,40+(i+2)%3*40,440+(i+2)/3*40,cx,"",i,12+i);
+	//12 ... 22
+	rep(i,0,13) addd(i%3*40,200+i/3*40,40+i%3*40,240+i/3*40,cn,n1[i],0,23+i);
+	//23 ... 36
+	addd(1220,600,1280,640,go,"计算",0,37);
+	//37
+	addd(1000,0,1280,560,ans,"",0,ansdomid=38);
+	//38
+	addd(980,600,1040,640,vxa,"时限",15,tlimdomid=39);
+	//39
+	addd(1100,600,1160,640,vxa,"目标",999,ehdomid=40);
+	//40
+	addd(0,0,400,40,lk,customlk,0,lkdomid=41);
+	//41
+	addd(0,40,60,80,lk1,"修改",0,42);
+	//42
+	addd(60,40,120,80,lk2,"读取",0,43);
+	//43
+	addd(0,80,120,120,vxb,"背刺可用",1,modedomid=44);
+	//44
+}
+
+kpm str2k(string s){
+	if(s=="暗步") return ayb;
+	if(s=="背刺") return bc;
+	if(s=="硬币") return jb;
+	if(s=="伺机") return sjdf;
+	if(s=="幻药") return hjys;
+	if(s=="鱼灵") return syzl;
+	if(s=="老千") return hrlq;
+	if(s=="刀油") return dy;
+	if(s=="腾武") return tw;
+	if(s=="鬼灵") return glfz;
+	if(s=="法术") return ljfs;
+	if(s=="随从") return ljsc;
+	if(s=="垃圾") return lj;
+	return lj;
+}
+kpm k2str(string s){
+	if(s=="暗步") return ayb;
+	if(s=="背刺") return bc;
+	if(s=="硬币") return jb;
+	if(s=="伺机") return sjdf;
+	if(s=="幻药") return hjys;
+	if(s=="鱼灵") return syzl;
+	if(s=="老千") return hrlq;
+	if(s=="刀油") return dy;
+	if(s=="腾武") return tw;
+	if(s=="鬼灵") return glfz;
+	if(s=="法术") return ljfs;
+	if(s=="随从") return ljsc;
+	if(s=="垃圾") return lj;
+	return lj;
+}
+
+int __eh=999;
+int __tlim=999999;
+
+state doms2st(vector<domain> a){
+	openmode=id2dom(modedomid).x;
+	
+	__eh=id2dom(ehdomid).x;
+	__tlim=id2dom(tlimdomid).x;
+	
+	state st;
+	st.hands.clear();
+	rep(i,0,9){
+		domain _card=id2dom(cardsdomid[i]);
+		if(_card.name!="清除") st.hands.push_back(cardcons(str2k(_card.name),_card.x));
+	}
+	st.fields.clear();
+	st.auras=emptyau;
+	st.mana=id2dom(manadomid).x;
+	st.num=0;
+	return st;
+}
+
+void st2doms(state st){
+	int p=0;
+	vector<card>::iterator i=st.hands.begin();
+	while(i!=st.hands.end()){
+		domain _card=id2dom(cardsdomid[p]);
+		_card.name=k2str((*i).name);
+		_card.x=(*i).cost;
+		follow(_card);
+		i++;
+		p++;
+	}
+	rep(i,p,9){
+		domain _card=id2dom(cardsdomid[i]);
+		_card.name="清除";
+		follow(_card);
+	}
+	domain _mana=id2dom(manadomid);
+	_mana.x=st.mana;
+	follow(_mana);
 }
 
 void drawdom(domain a,bool sl){
@@ -782,12 +930,17 @@ void drawdom(domain a,bool sl){
 			break;
 		}
 		case vnx:
-		case vx:{
-			textr(r,txc,a.name+to_string(a.x));
+		case vx:
+		case vxa:
+		case vxb:{
+			if(a.name=="清除") textr(r,txc,""); 
+			else textr(r,txc,a.name+to_string(a.x));
 			break;
 		}
 		case cn:
-		case go:{
+		case go:
+		case lk1:
+		case lk2:{
 			textr(r,txc,a.name);
 			break;
 		}
@@ -799,74 +952,11 @@ void drawdom(domain a,bool sl){
 			textra(r,txc,a.name);
 			break;
 		}
-	}
-}
-
-vector<domain> doms;
-domain selected;
-int manaid;
-int cardsdomid[10];
-int ansdomid;
-
-string n1[14]={"鱼灵","老千","刀油","鬼灵","腾武","暗步","背刺","硬币","伺机","幻药","随从","法术","垃圾",""};
-
-void getdoms(){
-	doms.clear();
-	addd(900,600,960,640,vx,"水晶",0,manaid=1);
-	//1
-	rep(i,0,9) addd(200+i*60,600,260+i*60,640,vnx,"",0,cardsdomid[i]=2+i);
-	//2 ... 11
-	rep(i,0,10) addd((i+2)%3*40,400+(i+2)/3*40,40+(i+2)%3*40,440+(i+2)/3*40,cx,"",i,12+i);
-	//12 ... 22
-	rep(i,0,13) addd(i%3*40,200+i/3*40,40+i%3*40,240+i/3*40,cn,n1[i],0,23+i);
-	//23 ... 36
-	addd(1060,560,1180,640,go,"开始计算",0,37);
-	//37
-	addd(1000,0,1280,560,ans,"",0,ansdomid=38);
-	//38
-}
-
-domain id2dom(int id){
-	vector<domain>::iterator i=doms.begin();
-	while(i!=doms.end()){
-		if(id==(*i).id){
-			return (*i);
+		case lk:{
+			textr(r,txc,a.name+"\\Logs\\power.log");
 			break;
-		}
-		i++;
+		} 
 	}
-	return nodomain;
-}
-
-kpm str2k(string s){
-	if(s=="暗步") return ayb;
-	if(s=="背刺") return bc;
-	if(s=="硬币") return jb;
-	if(s=="伺机") return sjdf;
-	if(s=="幻药") return hjys;
-	if(s=="鱼灵") return syzl;
-	if(s=="老千") return hrlq;
-	if(s=="刀油") return dy;
-	if(s=="腾武") return tw;
-	if(s=="鬼灵") return glfz;
-	if(s=="法术") return ljfs;
-	if(s=="随从") return ljsc;
-	if(s=="垃圾") return lj;
-	return lj;
-}
-
-state doms2st(vector<domain> a){
-	state st;
-	st.hands.clear();
-	rep(i,0,9){
-		domain _card=id2dom(cardsdomid[i]);
-		if(_card.name!="") st.hands.push_back(cardcons(str2k(_card.name),_card.x));
-	}
-	st.fields.clear();
-	st.auras=emptyau;
-	st.mana=id2dom(manaid).x;
-	st.num=0;
-	return st;
 }
 
 void loaddoms(){
@@ -879,16 +969,7 @@ void loaddoms(){
 	selected=nodomain;
 }
 
-void follow(domain a){
-	vector<domain>::iterator i=doms.begin();
-	while(i!=doms.end()){
-		if(a.id==(*i).id){
-			(*i)=a;
-			break;
-		}
-		i++;
-	}
-}
+state autoread(string s);
 
 void click(int x,int y){
 	domain toselect=nodomain;
@@ -906,7 +987,9 @@ void click(int x,int y){
 			break;
 		}
 		case vnx:
-		case vx:{
+		case vx:
+		case vxa:
+		case vxb:{
 			if(selected.id==toselect.id){
 				drawdom(selected,0);
 				selected=nodomain;
@@ -932,21 +1015,234 @@ void click(int x,int y){
 				drawdom(selected,1);
 				follow(selected);
 			}
+			if(selected.type==vxa){
+				if(toselect.x==0) selected.x=0;
+				else selected.x+=toselect.x;
+				drawdom(selected,1);
+				follow(selected);
+			}
+			if(selected.type==vxb){
+				selected.x=(bool)toselect.x;
+				drawdom(selected,1);
+				follow(selected);
+			}
 			break;
 		}
 		case go:{
 			state st=doms2st(doms);
 			domain ans=id2dom(ansdomid);
-			ans.name=solve(st);
+			ans.name=solve(st,__eh,__tlim);
 			drawdom(ans,0);
 			follow(ans);
+			break;
+		}
+		case ans:
+		case lk:{
+			
+			break;
+		}
+		case lk1:{
+			char _lk[100];
+        	inputbox_getline("请输入炉石传说本地路径（回车确认）","宇宙甜心",_lk,100);
+        	domain lk=id2dom(lkdomid);
+        	lk.name=(string)_lk;
+        	drawdom(lk,0);
+        	follow(lk);
+			break;
+		}
+		case lk2:{
+			state st=autoread(id2dom(lkdomid).name);
+			st2doms(st);
+			rep(i,0,9) drawdom(id2dom(cardsdomid[i]),0);
+			drawdom(id2dom(manadomid),0);
 			break;
 		}
 	}
 }
 
+namespace autor{
+	map<string,string> id2cid;
+	map<string,string> id2cost;
+	map<string,string> id2zone;
+	map<string,string> id2err;
+	
+	void getafter(string s,string a,string&t){
+		if(t!="") return;
+		int p=s.find(a);
+		if(p==-1) return;
+		p=p+a.size();
+		while(s[p]!=' '&&s[p]!='\0'){
+			t=t+s[p];p++;
+		}
+	}
+	void getid(string s,string&t){
+		getafter(s,"tag=ENTITY_ID value=",t);
+		getafter(s," id=",t);
+	}
+	void getcid(string s,string&t){
+		getafter(s,"CardID=",t);
+		getafter(s,"cardId=",t);
+	}
+	void getcost(string s,string&t){
+		getafter(s,"tag=COST value=",t);
+	}
+	void getzone(string s,string&t){
+		getafter(s,"tag=ZONE value=",t);
+		getafter(s,"zone=",t);
+	}
+	
+	int secrets;
+	
+	kpm cid2kpm(string s){
+		if(s.find("EX1_144")!=-1) return ayb;
+		if(s.find("CS2_072")!=-1) return bc;
+		if(s.find("CFM_630")!=-1) return jb;
+		if(s.find("COIN")!=-1) return jb;
+		if(s.find("GAME_005")!=-1) return jb;
+		if(s.find("EX1_145")!=-1) return sjdf;
+		if(s.find("SCH_352")!=-1) return hjys;
+		if(s.find("TRL_092")!=-1) return syzl;
+		if(s.find("DMF_511")!=-1) return hrlq;
+		if(s.find("BAR_552")!=-1) return dy;
+		if(s.find("DMF_071")!=-1) return tw;
+		if(s.find("ICC_910")!=-1) return glfz;
+		if(s.find("DMF_512")!=-1) return ljfs;
+		if(s.find("LOOT_214")!=-1&&secrets==0){
+			secrets=1;
+			return ljfs;
+		}
+		return lj;
+	}
+	
+	vector<string> hand;
+	state autost;
+	void autore(string _s){
+		autost=emptyst;
+		//现在可能多次调用，需要每次清空 
+		
+		//更新结构： 
+		//id->cid,cost,zone
+		//resource(mana)
+		//op->id,err
+		//此处为简便，直接令op.id->err 
+		
+		cin.clear();
+		//重置输入流，防止无法读取 
+		
+		freopen((_s+"\\Logs\\power.log").c_str(),"r",stdin);
+		
+		id2cid.clear();
+		id2cost.clear();
+		id2zone.clear();
+		id2err.clear();
+		hand.clear();
+		
+		string s,s0;
+		string s1,s2,s3,s4;
+		string s5,resources;
+		string s6;
+		
+		int lines=0;
+		
+		while(getline(cin,s)){
+			lines++;
+			if(s.find("- CREATE_GAME")!=-1){
+				id2cid.clear();
+				id2cost.clear();
+				id2zone.clear();
+				id2err.clear(); 
+				hand.clear();
+			}
+			
+			while(s.find("-     SHOW_ENTITY")!=-1){
+				getline(cin,s0);
+				while(s0.find("-         tag=")!=-1){
+					s=s+" "+s0;
+					getline(cin,s0);
+				}
+				s1=s2=s3=s4="";
+				getid(s,s1);
+				getcid(s,s2);
+				getcost(s,s3);
+				getzone(s,s4);
+				if(s1!=""){
+					if(s2!="") id2cid[s1]=s2;
+					if(s3!="") id2cost[s1]=s3;
+					if(s4!="") id2zone[s1]=s4;
+				}
+				
+				s=s0;
+				//s0不属于这部分却已经被读入了，应当直接视为下一次的s 
+				//且这个s可能立即属于新部分，故外层使用while 
+			}
+			
+			if(s.find("TAG_CHANGE")!=-1){
+				s1=s2=s3=s4=s5="";
+				getid(s,s1);
+				getcid(s,s2);
+				getcost(s,s3);
+				getzone(s,s4);
+				getafter(s,"tag=RESOURCES value=",s5);
+				if(s1!=""){
+					if(s2!="") id2cid[s1]=s2;
+					if(s3!="") id2cost[s1]=s3;
+					if(s4!="") id2zone[s1]=s4;
+				}
+				if(s5!="") resources=s5;
+				continue;
+			}
+			
+			if(s.find("Options() - id=")!=-1) hand.clear();
+			if(s.find("Options()")!=-1&&s.find("option")!=-1){
+				s1=s6="";
+				getid(s,s1);
+				getafter(s,"error=",s6);
+				if(s1!="") hand.push_back(s1);
+				if(s1!=""&&s6!="") id2err[s1]=s6;
+				//此处有可能存在error=项但项为空，不必区分 
+			}
+		}
+		
+		secrets=0;
+		vector<string>::iterator i=hand.begin();
+		while(i!=hand.end()){
+			s1=*i;
+			s2=id2cid[s1];
+			s3=id2cost[s1];
+			if(s3=="") s3="0";
+			s4=id2zone[s1];
+			if(s4=="HAND"||s4=="DECK"){
+				//偶尔手牌会保持显示zone=DECK 
+				//虽然很奇怪但能work 
+				autost.hands.push_back(cardcons(cid2kpm(s2),atoi(s3.c_str())));
+			}
+			s6=id2err[s1];
+			if(cid2kpm(s2)==bc&&s6!=""){
+				openmode=0;
+				//模式检定，将无法直接通过bc->nul 
+			}
+			else openmode=1;
+			
+			id2zone[s1]="";
+			//防止（可交易牌）被统计两次 
+			i++;
+		}
+		
+		autost.mana=atoi(resources.c_str());
+		
+		cout<<lines<<endl;
+		
+		fclose(stdin);
+	}
+}
+using namespace autor;
+state autoread(string s){
+	autore(s);
+	return autost;
+}
+
 int main(){
-	ShowWindow(GetForegroundWindow(),0);
+	//ShowWindow(GetForegroundWindow(),0);
 	
 	init();
 	attention();
